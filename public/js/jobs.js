a2b00a9695d179f30a5310cc809e584284e135a2
Plamen Kostov <PKKostov18@codingburgas.bg>
2021-07-15 01:14:36 +0300

    Implement job posting and visualize the database information Fix #27 Fix #26 Fix #25 Fix #24 Fix #23 Fix #21

---
+++ public/js/jobs.js
@@ -0,0 +1,35 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+const btnToScrollTop = document.getElementById("btnToScrollTop")
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnToScrollTop.style.display = "block";
+    } else {
+        btnToScrollTop.style.display = "none";
+    }
+  }
+
+btnToScrollTop.addEventListener("click", function topFunction() {
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+} )
