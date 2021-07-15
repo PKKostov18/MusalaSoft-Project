8fa9f512a205a40fc80a326ce176e97f8a9413bb
Plamen Kostov <PKKostov18@codingburgas.bg>
2021-07-15 23:50:48 +0300

    Add the new stored procedure

--- Database/login_stored_procedure.sql
+++ Database/login_stored_procedure.sql
@@ -3,14 +3,15 @@ CREATE OR ALTER PROCEDURE LoginUser
 @Username varchar(30),
 @IncommingPassword nvarchar(256),
 @VerifiedId int OUTPUT,
-@UsernameOut varchar(30) OUTPUT
+@UsernameOut varchar(30) OUTPUT,
+@PasswordOut nvarchar(256) OUTPUT
 AS
 
 DECLARE @UserPassword nvarchar(256)
 
-SELECT @UserPassword = [Password], @VerifiedId = Id, @UsernameOut = Username
+SELECT @UserPassword = [Password], @VerifiedId = Id, @UsernameOut = Username, @PasswordOut = [Password]
 	From Users
 WHERE Username = @Username
 
-IF @IncommingPassword <> @UserPassword
+IF @IncommingPassword <> @UserPassword 
 	SET @VerifiedId = 0
